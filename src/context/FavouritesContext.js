c5bac04eb3e97ffc900f770f61b2e5c5d1ab1434
ujvarivera <ujvariveronika0320@gmail.com>
2021-11-02 20:48:56 +0100

    LocalStorage added

--- src/context/FavouritesContext.js
+++ src/context/FavouritesContext.js
@@ -1,21 +1,29 @@
 import React, { createContext, useReducer } from "react";
 import { initialState, reducer, ACTIONS } from "../reducers/FavouriteReducer";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const Context = createContext();
 
 export default function FavouritesContext({ children }) {
-  const [favourites, dispatch] = useReducer(reducer, initialState);
+  const { deleteValue, addValue, state, resetValue } = useLocalStorage(
+    "favourites",
+    initialState
+  );
+  const [favourites, dispatch] = useReducer(reducer, state);
 
   const like = (meal) => {
     dispatch({ type: ACTIONS.LIKE, payload: meal });
+    addValue(meal);
   };
 
   const unlike = (meal) => {
     dispatch({ type: ACTIONS.UNLIKE, payload: meal });
+    deleteValue(meal);
   };
 
   const reset = () => {
     dispatch({ type: ACTIONS.RESET });
+    resetValue();
   };
 
   const values = {
