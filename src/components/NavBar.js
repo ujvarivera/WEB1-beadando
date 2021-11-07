73749c277f3379de47fe3b4fe6b4c2b38a6219c5
ujvarivera <ujvariveronika0320@gmail.com>
2021-11-07 17:52:10 +0100

    styles updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
 
+const selectedStyle = {
+  fontWeight: "bold",
+  }
+
 export default function Header() {
   return (
     <div className="navbar">
       <NavLink to="/">
         <h1>Meals</h1>
       </NavLink>
-        <NavLink to="/">Back to Home</NavLink>
-        <NavLink to="/favourites">Favourites</NavLink>
+        <NavLink to="/" exact activeStyle={selectedStyle}>Back to Home</NavLink>
+        <NavLink to="/favourites" exact activeStyle={selectedStyle}>Favourites</NavLink>
     </div>
   );
 }
